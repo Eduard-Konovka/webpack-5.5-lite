b53fc58349d103074aa22e0f1d9db8faa27d0938
Eduard-Konovka <ed098ua@gmail.com>
2021-09-14 15:56:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 import './css/styles.scss'
 import './images/sprite.svg'
 import db from './db/db.json'
-import templ from './templates/templ.hbs'
+import templ from './templates/templ.handlebars'
 import refs from './js/refs'
 import './js/func'
 import './js/listeners'
